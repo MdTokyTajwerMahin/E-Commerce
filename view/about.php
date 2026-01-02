0f41c9d5003d0e472499db66dee5fa694b71ef41
MdTokyTajwerMahin <tokitajwermahin268@gmail.com>
2026-01-02 14:31:57 +0600

    about & contact

---
+++ view/about.php
@@ -0,0 +1,69 @@
+<?php
+    //session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About Us</title>
+    <link rel="stylesheet" href="/Project/CSS/about.css">
+</head>
+<body>
+<header>
+    <h2>ALIDADA</h2>
+    <nav>
+        <a href="../index.php">Home</a>
+        <a href="index.php">Sign Up</a>
+        <a href="Login.php">Login</a>
+        <a href="contact.php">Contact</a>
+    </nav>
+</header>
+
+<hr>
+
+<div class="about-section">
+    <h2>About Us</h2>
+
+
+    <div class="info-box">
+        <div class="info-text">
+            <h3>Developer</h3>
+            <p><strong>Azaz Mohammad Kaif</strong><br>
+               Student ID: 22-48795-3 <br>
+               <strong>American International University Bangladesh (AIUB)</strong></p>
+        </div>
+        <img src="/Project/Image/kaif.jpg" alt="Azaz Mohammad Kaif">
+    </div>
+
+
+    <div class="info-box">
+        <div class="info-text">
+            <h3>Developer</h3>
+            <p><strong>Majedul Islam</strong><br>
+               Student ID: 22-48618-3 <br>
+               <strong>American International University Bangladesh (AIUB)</strong></p>
+        </div>
+        <img src="/Project/Image/anas.jpg" alt="Majedul Islam">
+    </div>
+
+
+    <div class="info-box">
+        <div class="info-text">
+            <h3>Supervisor</h3>
+            <p><strong>Wahidul Alam Riyad</strong><br>
+               Lecturer, CS<br>
+               <strong>American International University Bangladesh (AIUB)</strong></p>
+        </div>
+        <img src="/Project/Image/supervisor.jpg" alt="Supervisor">
+    </div>
+</div>
+
+<br><br>
+<footer>
+        <hr>
+        <p>Copyright &copy; All rights reserved by ALIDADA</p>
+</footer>
+</body>
+</html>
